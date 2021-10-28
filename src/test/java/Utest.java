34ba210b778c992bf7a84c326742115b6ab16391
hunata <hunata87@gmail.com>
2021-10-28 19:15:32 +0700

    compplete

--- src/test/java/Utest.java
+++ src/test/java/Utest.java
@@ -1,3 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import entities.Student;
+
 public class Utest {
 
+    @Test
+    @DisplayName("shouldDisplay10Students")
+    void shouldDisplay10Students() {
+        
+    }
 }
